4e73893f09c9bf6309dce9454f2b402e58c0df5b
FadedLocks702 <31429405+FadedLocks702@users.noreply.github.com>
2017-10-19 16:42:09 -0700

    startin to come together

--- JS/script.js
+++ JS/script.js
@@ -13,5 +13,20 @@ $("#Malibu-2013").click(showMalibu);
 //3. USER CLICKS ANOTHER LINK ON THE LEFT NAV, and CORRESPONDING BLOG PAGES
 //COMES OUT
 
+//function showLA01
+//function showHollywood01
+//function showSantaB01
+//function showBWhalf01
+//function showLA02
+//function showOCHalf01
+//function showSFFull01
+//function showSMClassic2015
+//function showLBFull01
+//function showCIM01
+//function showSurfCity01
+//function showHollywood02
+//function showSMClassic2016
+//function showPasadena01
+
 
 });
