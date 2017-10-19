5b6fe7547f7d5257518dae94bb123a77ae82fceb
FadedLocks702 <31429405+FadedLocks702@users.noreply.github.com>
2017-10-19 01:43:44 -0700

    Page is coming around
    Fleshing out the html and css more.. Begun javascript template

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+//blog
+//past-races page
+$( document ).ready(function(){
+
+
+//1. DEFAULT: NO BLOG PAGES SHOWN IN THE FRAME
+//2. USER CLICKS LEFT NAV, and BLOG PAGE SLIDES OUT
+function showMalibu(){
+	$("#Malibu-Half").show()
+}
+$("#Malibu-2013").click(showMalibu);
+
+//3. USER CLICKS ANOTHER LINK ON THE LEFT NAV, and CORRESPONDING BLOG PAGES
+//COMES OUT
+
+
+});
