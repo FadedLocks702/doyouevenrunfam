cdfb8e6307675029494c81edb3ba4c405c81ebc4
FadedLocks702 <31429405+FadedLocks702@users.noreply.github.com>
2017-10-26 00:15:15 -0700

    Major Changes
    Messed around with figuring out how to make the past races page dynamic.  I got some help with creating a 'data-race' solution to the links and the 'active' state ID.
    
    #1 - I made the left nav into regular links. Then I gave the function dutire using the 'on' event.
    #2 - I gave a default for the main content section to ALWAYS remove the current page so every time any of the links on the left were clicked.

--- JS/script.js
+++ JS/script.js
@@ -1,128 +1,117 @@
-$( document ).ready(function(){
-
-//instagram profile//
+$(function()	{
 
 
-
-//blog
-//past-races page
 
 //1. DEFAULT: NO BLOG PAGES SHOWN IN THE FRAME 
-//		NEW DESIGN: all blog pages are shown
+
+// first iteration
+		// var race = $(this).data('race');
+
+		// $('#main-content section').each(function() {
+		// 	var content = $(this).attr('id');
+		// 	console.log(content);
+
+		// 	if (content === race) {
+		// 		$(this).addClass('active');
+		// 	} else {
+		// 		$(this).removeClass('active');
+		// 	}
+
+
+//optimized
+
+	$('.list-of-races').on('click','a', function(e)	{
+		e.preventDefault();
+	$('#main-content section').removeClass('active');
+	$('#' + $(this).data('race')).addClass('active');
+
+});
+
+
 //2. USER CLICKS LEFT NAV, and BLOG PAGE SLIDES OUT
-//		NEW DESIGN - LEFT NAV SCROLLS TO ANCHOR
-function showMalibu(){
-	$("#malibuHalf").fadeToggle();
-	}
-	$(".Malibu-2013").click(showMalibu);
 
-function showLA2014(){
-	$("#laMar-01").fadeToggle();
-	}
-	$(".LAMar-2014").click(showLA2014);
-
-function showHHalf01(){
-	$("#hollyHf-01").fadeToggle();
-	}
-	$(".HollyH-2014").click(showHHalf01);
-
-function santaB01(){
-	$("#sBar-01").fadeToggle();
-	}
-	$(".SBar-2014").click(santaB01);
 
 //sample:
 
-// function showDetail(){
-// 	$('specificSections').fadeToggle();
-// 	}
-// 	$(".list-of-races").click('specificIDs')
-
-// 1 possible solution
-// function raceDetail(){
-// 	$("#bWorld-01","#laMar02").fadeToggle();
-// 	}
-// 	$(".BWorld-2015",".LA-Mar-2015").click(raceDetail);
-// console.log(raceDetail);
-
-// function ShowBWorld(){
-// 	$('#bWorld-01').fadeToggle();
-// }
-// function ShowLAMarathon02(){
-// 	$('#laMar-02').fadeToggle();
-// }
-
-
 //FALL BACK ON CODE BELOW IF IT DOESNT WORK
-function showBetter01(){
-	$("#bWorld-01").fadeToggle();
-	}
-	$(".BWorld-2015").click(showBetter01);
 
-function showLA2015(){
-	$("#laMar-02").fadeToggle();
-	}
-	$(".LAMar-2015").click(showLA2015);
+// function showMalibu(){
+// 	$("#malibuHalf").fadeToggle();
+// 	}
+// 	$(".Malibu-2013").click(showMalibu);
 
-function showOC01(){
-	$("#oC-01").fadeToggle();
-	}
-	$(".OCHa-2015").click(showOC01);
+// function showLA2014(){
+// 	$("#laMar-01").fadeToggle();
+// 	}
+// 	$(".LAMar-2014").click(showLA2014);
 
-function showSF01(){
-	$("#sFran-01").fadeToggle();
-	}
-	$(".SFMar-2015").click(showSF01);
+// function showHHalf01(){
+// 	$("#hollyHf-01").fadeToggle();
+// 	}
+// 	$(".HollyH-2014").click(showHHalf01);
 
-function showLB01(){
-	$("#lbeach-01").fadeToggle();
-	}
-	$(".LBMar-2015").click(showLB01);
+// function santaB01(){
+// 	$("#sBar-01").fadeToggle();
+// 	}
+// 	$(".SBar-2014").click(santaB01);
 
-function showSac01(){
-	$("#sac-01").fadeToggle();
-	}
-	$(".CIM-2015").click(showSac01);
 
-function showSurfCity01(){
-	$("#surfCity-01").fadeToggle();
-	}
-	$(".HBSurfCi-2016").click(showSurfCity01);
+// function showBetter01(){
+// 	$("#bWorld-01").fadeToggle();
+// 	}
+// 	$(".BWorld-2015").click(showBetter01);
 
-function showHollywood02(){
-	$("#hollyHf-02").fadeToggle();
-	}
-	$(".HollyH-2016").click(showHollywood02);
+// function showLA2015(){
+// 	$("#laMar-02").fadeToggle();
+// 	}
+// 	$(".LAMar-2015").click(showLA2015);
 
-function showPasadena01(){
-	$("#Passa-01").fadeToggle();
-	}
-	$(".PasHalf-2017").click(showPasadena01);
+// function showOC01(){
+// 	$("#oC-01").fadeToggle();
+// 	}
+// 	$(".OCHa-2015").click(showOC01);
 
-function showLAMarathon03(){
-	$("#laMar-03").fadeToggle();
-	}
-	$(".LAMar-2017").click(showLAMarathon03);
+// function showSF01(){
+// 	$("#sFran-01").fadeToggle();
+// 	}
+// 	$(".SFMar-2015").click(showSF01);
 
-function showSF02(){
-	$("#sFran-02").fadeToggle();
-	}
-	$(".SFHaMar-2017").click(showSF02);
+// function showLB01(){
+// 	$("#lbeach-01").fadeToggle();
+// 	}
+// 	$(".LBMar-2015").click(showLB01);
+
+// function showSac01(){
+// 	$("#sac-01").fadeToggle();
+// 	}
+// 	$(".CIM-2015").click(showSac01);
+
+// function showSurfCity01(){
+// 	$("#surfCity-01").fadeToggle();
+// 	}
+// 	$(".HBSurfCi-2016").click(showSurfCity01);
+
+// function showHollywood02(){
+// 	$("#hollyHf-02").fadeToggle();
+// 	}
+// 	$(".HollyH-2016").click(showHollywood02);
+
+// function showPasadena01(){
+// 	$("#Passa-01").fadeToggle();
+// 	}
+// 	$(".PasHalf-2017").click(showPasadena01);
+
+// function showLAMarathon03(){
+// 	$("#laMar-03").fadeToggle();
+// 	}
+// 	$(".LAMar-2017").click(showLAMarathon03);
+
+// function showSF02(){
+// 	$("#sFran-02").fadeToggle();
+// 	}
+// 	$(".SFHaMar-2017").click(showSF02);
 
 	});
 
-//Click handler
 
 
-//Show function
-
-//3. USER CLICKS ANOTHER LINK ON THE LEFT NAV, and CORRESPONDING BLOG PAGES
-//COMES OUT.  
-
-//attemptinng an array
-// var races = ["#HollyH-2014","#SBar-2014","#BWorld-2015","#LAMar-2015","#OCHa-2015"];
-
-// 	for (var i=0; i<races.length; i++){
-// 		$('.list-of-races').val(races[i]).html(races[i]).appendTo('<article/>');
-// 		console.log($('.list-of-races').val(races[i]));
-// 	}
